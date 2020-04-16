3a6082d2fc4f0b806dd64bf17717f7b27a53233b
Gianmaria Del Monte <g.macmount@gmail.com>
2020-04-16 21:52:48 +0200

    user defined unary op

--- src/unaryop.jl
+++ src/unaryop.jl
@@ -1,20 +1,38 @@
 mutable struct GrB_UnaryOp
     p::Ptr{Cvoid}
-    str::String
+    fun::Function
 
     function GrB_UnaryOp(str::String)
-        return new(load_global(str), str)
+        return new(load_global(str))
     end
+
+    function GrB_UnaryOp(fun::Function)
+        return new(C_NULL, fun)
+    end
+    
 end
 
-global_unaryop = Dict{Symbol, Array{GrB_UnaryOp, 1}}()
+const global_unaryop = Dict{Symbol, Array{GrB_UnaryOp, 1}}()
 
-grb_uop = compile(["GrB"],
-        ["IDENTITY", "AINV", "MINV"],
-        ["BOOL", "UINT8", "UINT16", "UINT32", "UINT64", "INT8", "INT16", "INT32", "INT64", "FP32", "FP64"])
+function unaryop(s::Symbol, fun::Function)
+    if haskey(global_unaryop, s)
+        error("unary operation already exists")
+    else
+        push!(global_unaryop, s => [GrB_UnaryOp(fun)])
+    end
+    nothing
+end
 
-gxb_uop = compile(["GxB"],
-        ["ONE", "ABS"],
-        ["BOOL", "UINT8", "UINT16", "UINT32", "UINT64", "INT8", "INT16", "INT32", "INT64", "FP32", "FP64"])
+function load_builtin_unaryop()
+    global global_unaryop
 
-load_buildin_op(global_unaryop, cat(grb_uop, gxb_uop, dims=1), GrB_UnaryOp)
+    grb_uop = compile(["GrB"],
+    ["IDENTITY", "AINV", "MINV"],
+    ["BOOL", "UINT8", "UINT16", "UINT32", "UINT64", "INT8", "INT16", "INT32", "INT64", "FP32", "FP64"])
+
+    gxb_uop = compile(["GxB"],
+    ["ONE", "ABS"],
+    ["BOOL", "UINT8", "UINT16", "UINT32", "UINT64", "INT8", "INT16", "INT32", "INT64", "FP32", "FP64"])
+
+    load_buildin_op(global_unaryop, cat(grb_uop, gxb_uop, dims=1), GrB_UnaryOp)
+end
