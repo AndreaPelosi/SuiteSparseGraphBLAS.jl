72c6c84e0529a46304ab0e35507b19c3351b60b4
Gianmaria Del Monte <g.macmount@gmail.com>
2020-04-19 19:42:52 +0200

    monoid

---
+++ src/monoid.jl
@@ -0,0 +1,84 @@
+mutable struct GrB_Monoid
+    p::Ptr{Cvoid}
+    domain::GType
+
+    GrB_Monoid() = new()
+    GrB_Monoid(op, domain) = new(load_global(op), domain)
+end
+
+mutable struct Monoid
+    ops::Array{GrB_Monoid,1}
+
+    Monoid() = new([])
+end
+
+const Monoids = Dict{Symbol,Monoid}()
+
+Base.push!(m::Monoid, items...) = push!(m.ops, items...)
+
+# create new monoid from binary operation and identity value
+function monoid(s::Symbol, bin_op::BinaryOperation, identity::T) where T <: valid_types
+    domain = j2gtype(T)
+    monoid = get!(Monoids, s, Monoid())
+    index = findfirst(m->m.domain == domain, monoid.ops)
+    if index == nothing
+        bop = get_binaryop(bin_op, domain, domain, domain)
+        grb_monoid = GrB_Monoid_new(bop, identity)
+        push!(monoid, grb_monoid)
+    else
+        error("monoid already exists")
+    end
+    return monoid
+end
+
+function load_builtin_monoid()
+
+    function load(lst)
+        for op in lst
+            bpn = split(op, "_")
+            type = str2gtype(string(bpn[end]))
+            
+            monoid_name = Symbol(join(bpn[2:end - 1]))
+            monoid = get!(Monoids, monoid_name, Monoid())
+            push!(monoid, GrB_Monoid(op, type))
+        end
+    end
+
+    grb_mon = compile(["GrB"],
+    ["MIN", "MAX", "PLUS", "TIMES"],
+    ["BOOL", "UINT8", "UINT16", "UINT32", "UINT64", "INT8", "INT16", "INT32", "INT64", "FP32", "FP64"])
+
+    grb_mon_bool = compile(["GxB"],
+    ["LOR", "LAND", "LXOR", "ISEQ"],
+    ["BOOL"])
+
+    load(cat(grb_mon, grb_mon_bool, dims = 1))
+        
+end
+
+function Base.getproperty(d::Dict{Symbol,Monoid}, s::Symbol)
+    try
+        return getfield(d, s)
+    catch
+        return d[s]
+    end
+end
+
+function GrB_Monoid_new(binary_op::GrB_BinaryOp, identity::T) where T
+    monoid = GrB_Monoid()
+    monoid.domain = j2gtype(T)
+
+    monoid_ptr = pointer_from_objref(monoid)
+    fn_name = "GrB_Monoid_new_" * suffix(T)
+
+    check(GrB_Info(ccall(dlsym(
+        graphblas_lib, fn_name),
+        Cint,
+        (Ptr{Cvoid}, Ptr{Cvoid}, Cintmax_t),
+        monoid_ptr,
+        binary_op.p,
+        identity)
+        )
+    )
+    return monoid
+end
